60a8fbaa9610a33c0c79e92c1a09cfe7d10c217b
Jérémy Leherpeur <amenophis@leherpeur.net>
2020-10-28 21:35:18 +0100

    WIP

--- src/Infrastructure/Security/LoginFormUserAuthenticator.php
+++ src/Infrastructure/Security/LoginFormUserAuthenticator.php
@@ -6,20 +6,19 @@ use App\Domain\Data\Model\Exception\UserNotFound;
 use App\Domain\Data\Repository\Users;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
-use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 
-class LoginFormUserAuthenticator extends AbstractFormLoginAuthenticator
+class LoginFormUserAuthenticator extends AbstractAuthenticator
 {
-    use TargetPathTrait;
-
     public const LOGIN_ROUTE = 'login';
 
     private Users $users;
@@ -33,49 +32,40 @@ class LoginFormUserAuthenticator extends AbstractFormLoginAuthenticator
         $this->passwordEncoder = $passwordEncoder;
     }
 
-    public function supports(Request $request)
+    public function supports(Request $request): ?bool
     {
         return self::LOGIN_ROUTE === $request->attributes->get('_route')
             && $request->isMethod('POST')
         ;
     }
 
-    public function getCredentials(Request $request)
+    public function authenticate(Request $request): PassportInterface
     {
-        $credentials =  [
-            'email' => $request->request->get('email'),
-            'password' => $request->request->get('password'),
-        ];
 
-        $request->getSession()->set(
-            Security::LAST_USERNAME,
-            $credentials['email']
-        );
 
-        return $credentials;
-    }
-
-    public function getUser($credentials, UserProviderInterface $userProvider)
-    {
         try {
-            return User::createFromUser($this->users->getByEmail($credentials['email']));
+            $user = User::createFromUser($this->users->getByEmail($request->request->get('email')));
+
+            return new Passport($user, new PasswordCredentials($request->request->get('password')), [
+//                // and CSRF protection using a "csrf_token" field
+//                new CsrfTokenBadge('loginform', $request->get('csrf_token')),
+//
+//                // and add support for upgrading the password hash
+//                new PasswordUpgradeBadge($request->get('password'), $this->userRepository)
+            ]);
+
         } catch (UserNotFound $e) {
             throw new CustomUserMessageAuthenticationException('Email could not be found.', [], 0, $e);
         }
     }
 
-    public function checkCredentials($credentials, UserInterface $user)
-    {
-        return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
-    }
-
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
         return new RedirectResponse($this->router->generate('homepage'));
     }
 
-    protected function getLoginUrl()
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        return $this->router->generate(self::LOGIN_ROUTE);
+        return null;
     }
 }
